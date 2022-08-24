01011bbdc53cb38378fa1ecb7ffd87be93ccbce5
Luis Saavedra <lui.saavedra@duocuc.cl>
2022-08-24 13:21:27 -0400

    finalización de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
+  
+  //CREACIÓN DEL ARREGLO PARA CAPTURAR LOS DATOS DEL USUARIO.
+  usuario = {
+    nombre:'',
+    password:''
+  };
   constructor() { }
 
   ngOnInit() {
+  }
+  //CREACIÓN DE FUNCIÓN QUE SE EJECUTARÁ AL MOMENTO DE HACER SUBMIT EN EL FORMULARIO.
+  onSubmitTemplate(){
+    console.log("guardado")
+    console.log(this.usuario)
   }
 
 }
